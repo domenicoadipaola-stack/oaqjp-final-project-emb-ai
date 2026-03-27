0c08ffcc8708e04592062900e28d67337bda43c1
Domenico DiPaola <domenico.a.dipaola@gmail.com>
2026-03-27 15:49:55 -0400

    udded server

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/emotionDetector')
+def emotion_detector_route():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    # Format and return the response
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
